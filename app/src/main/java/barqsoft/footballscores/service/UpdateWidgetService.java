cf01d09072f089ea97f9d4ca3364bd018fae24fc
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-04 21:39:54 +0530

    Created an Empty Widget

---
+++ app/src/main/java/barqsoft/footballscores/service/UpdateWidgetService.java
@@ -0,0 +1,52 @@
+package barqsoft.footballscores.service;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+
+/**
+ * Created by Aditya on 04-Mar-16.
+ */
+public class UpdateWidgetService extends IntentService {
+
+    public static final String TAG = UpdateWidgetService.class.getSimpleName();
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public UpdateWidgetService(String name) {
+        super(name);
+    }
+
+    public UpdateWidgetService() {
+        super(TAG);
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this.getApplicationContext());
+
+        int[] appWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            int id = appWidgetIds[i];
+
+            Intent inten = new Intent(this.getApplicationContext(), MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this.getApplicationContext(), 0, inten, 0);
+
+            RemoteViews views = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_frame);
+            views.setOnClickPendingIntent(R.id.frame_widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(id, views);
+
+        }
+    }
+}
