b584c1375b1a74a64c4cf5fa059c7b3d8cbd5cb3
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-05 18:37:01 +0530

    Added data to widget from content resolver

--- app/src/main/java/barqsoft/footballscores/service/UpdateWidgetService.java
+++ app/src/main/java/barqsoft/footballscores/service/UpdateWidgetService.java
@@ -4,15 +4,29 @@ import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.MainActivity;
 import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
 
 /**
  * Created by Aditya on 04-Mar-16.
  */
 public class UpdateWidgetService extends IntentService {
+
+    public static final int COL_HOME = 3;
+    public static final int COL_AWAY = 4;
+    public static final int COL_HOME_GOALS = 6;
+    public static final int COL_AWAY_GOALS = 7;
+    public static final int COL_DATE = 1;
+    public static final int COL_LEAGUE = 5;
+    public static final int COL_MATCHDAY = 9;
+    public static final int COL_ID = 8;
+    public static final int COL_MATCHTIME = 2;
 
     public static final String TAG = UpdateWidgetService.class.getSimpleName();
 
@@ -45,10 +59,19 @@ public class UpdateWidgetService extends IntentService {
             RemoteViews views = new RemoteViews(this.getApplicationContext().getPackageName(), R.layout.widget_frame);
 
 
-            views.setTextViewText(R.id.home_name,"Home");
-            views.setTextViewText(R.id.score_textview,"0-1");
-            views.setTextViewText(R.id.away_name,"Away");
-            views.setTextViewText(R.id.data_textview,"This is some data");
+            Cursor cursor = getContentResolver().query(DatabaseContract.BASE_CONTENT_URI, null, null, null, null);
+
+            Log.d("Widget", String.valueOf(cursor.getCount()));
+            cursor.moveToFirst();
+            assert cursor != null;
+            String score = Utilies.getScores(cursor.getInt(COL_HOME_GOALS), cursor.getInt(COL_AWAY_GOALS));
+
+            views.setTextViewText(R.id.home_name, cursor.getString(COL_HOME));
+            views.setTextViewText(R.id.score_textview, score);
+            views.setTextViewText(R.id.away_name, cursor.getString(COL_AWAY));
+            views.setTextViewText(R.id.data_textview, cursor.getString(COL_MATCHTIME));
+            views.setImageViewResource(R.id.home_crest, R.drawable.ic_launcher);
+            views.setImageViewResource(R.id.away_crest, R.drawable.ic_launcher);
 
             views.setOnClickPendingIntent(R.id.frame_widget, pendingIntent);
 
